cccb4c031caff5e0f0bf8787c49ecea0221ec0da
Chandan Kumar Mandal <chankruze@gmail.com>
2022-04-06 10:37:01 +0530

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+/*
+Author: chankruze (chankruze@geekofia.in)
+Created: Tue Nov 16 2021 20:14:01 GMT+0530 (India Standard Time)
+
+Copyright (c) Geekofia 2021 and beyond
+*/
+
+// If you want to use other PostCSS plugins, see the following:
+// https://tailwindcss.com/docs/using-with-preprocessors
+module.exports = {
+    plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+};
