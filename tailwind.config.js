774ec8fd7e8772a7b14289c5e642efcff255138c
chankruze <chankruze@gmail.com>
2022-05-06 18:21:58 +0530

    ui: 💄 extends tailwind colors
    Signed-off-by: chankruze <chankruze@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,25 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-    mode: "jit",
-    purge: [
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
-    ],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {
-            fontFamily: {
-                poppins: ["Poppins", "sans-serif"],
-                roboto: ["Roboto", "sans-serif"],
-                nunito: ["Nunito", "sans-serif"],
-                montserrat: ["Montserrat", "sans-serif"],
-            },
-        },
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+        nunito: ["Nunito", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        ...colors,
+      },
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
